b92fc5838cefa1683b5537670d8e904da4335ab7
Amir Abdallah <amir21abdallah@gmail.com>
2024-02-11 16:18:54 +0100

    i just add my pagest routers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import SignUp from './pages/SignUp';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className='text-3xl'>Hello THERE </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div>
+        <h1>hello Amir </h1>
+
+
+    </div>
+  )
+}
+
+export default SignUp
